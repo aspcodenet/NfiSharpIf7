05068b1b8e938746e91ad14d728034adfce7f35a
Stefan Holmberg <stefan.holmberg@systementor.se>
2021-05-10 16:40:45 +0200

    Add project files.

---
+++ NfiSharpIf7/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NfiSharpIf7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Skriv in ditt land");
+            var land = Console.ReadLine();
+
+
+            
+
+            if (land == "Sverige")
+            {
+                Console.WriteLine("Du bor i Skandinavien");
+            }
+            else if (land == "Norge")
+            {
+                Console.WriteLine("Du bor i Skandinavien");
+            }
+            else if (land == "Danmark")
+            {
+                Console.WriteLine("Du bor i Skandinavien");
+            }
+            else
+                Console.WriteLine("Du bor INTE i Skandinavien");
+
+
+
+            if (land == "Sverige" || land == "Danmark" || land == "Norge")
+            {
+                Console.WriteLine("Du bor i Skandinavien");
+            }
+            else
+                Console.WriteLine("Du bor INTE i Skandinavien");
+        }
+    }
+}
